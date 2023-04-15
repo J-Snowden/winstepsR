f11ec7eec1c0e4f277016e46441879d17c396f22
Jeffrey <jeffreysnowden@gmail.com>
2023-04-15 12:00:09 -0500

    added package documentation

---
+++ R/winstepsR.R
@@ -0,0 +1,18 @@
+#' winstepsR: A package interfacing R with the Winsteps program.
+#'
+#' The winstepsR package interfaces with Winsteps to run control files
+#' and export tables and files from R.
+#'
+#' @section winstepsR functions:
+#' \link[winstepsR]{filter_zresid}
+#'
+#' r_control_file
+#'
+#' winsteps_output
+#'
+#' clean_dif
+#'
+#' @docType package
+#' @name winstepsR
+NULL
+#> NULL
