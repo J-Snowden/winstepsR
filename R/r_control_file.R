966d536ff8d4955ca8ee99a0f8c9c374f6333ef0
Jeffrey <jeffreysnowden@gmail.com>
2023-04-15 10:57:24 -0500

    added documentation

--- R/r_control_file.R
+++ R/r_control_file.R
@@ -1,15 +1,33 @@
+#' @title r_control_file
+#'
+#' @description Creates a Winsteps control file from an R data.frame
+#'
+#' @param name The name you want to save the control file as
+#' @param data A data set object
+#' @param item1 The numeric location of the first item in your dataset
+#' @param ni The number of items you want to add to the control file
+#' @param id_col The name of the column where person names are located
+#' @param groups Optional. String input for addings a ISGROUPS line.
+#' @param ifile Optional. Location of a .txt ifile
+#' @param sfile Optional. Location of a .txt sfile
+#' @param demographics Optional. Column names of demographic variables to
+#' include in the control file
+#' @return Creates a Winsteps control file in the current directory
+#' @export
+#' @importFrom dplyr "%>%"
+
 r_control_file <- function(name, data, item1, ni, id_col, ..., groups = NULL,
                            ifile = NULL, sfile = NULL, demographics = NULL) {
-  
+
   tic("generating control file")
- 
+
   groups_string <- ""
-  
+
   data <- data %>%
     select(all_of(item1):(all_of(item1)+(ni-1)), all_of(id_col)) %>%
     mutate("blank" = " ") %>%
     relocate(all_of(id_col), .after = "blank")
-  
+
   if (!is.null(demographics)) {
     demographics <- demographics %>%
       mutate(across(everything(), as.character))
@@ -17,24 +35,24 @@ r_control_file <- function(name, data, item1, ni, id_col, ..., groups = NULL,
     demographics[is.na(demographics)] <- "."
 
     #do the next 2 lines need to be in here? Or are they better outside?
-    data$NewSID <- stringr::str_pad(data$NewSID, max(nchar(data[[id_col]])), 
+    data$NewSID <- stringr::str_pad(data$NewSID, max(nchar(data[[id_col]])),
                                     "right")
-    
+
     data$NewSID <- paste0(str_c(data$NewSID, ' '))
-    
+
     for (i in 1:length(demographics)) {
       data$NewSID <- paste0(str_c(data$NewSID, eval(parse(text = paste(
         "demographics$", colnames(demographics[i]))))), " ")
-      
+
     }
-    
+
   } else {demographics <- c()
   }
-  
+
   name1 <- grep(id_col, colnames(data))
   namlen <- max(nchar(data[[id_col]]))
   xwide <- length(max(data[1:(1+(ni-1))], na.rm = TRUE))
-  
+
   out_string1 <- paste0("&INST\n",
                         'Title= "', name, ' - generated in R"\n',
                         "; Created: ", Sys.time(), "\n",
@@ -47,7 +65,7 @@ r_control_file <- function(name, data, item1, ni, id_col, ..., groups = NULL,
                         "NAMLEN = ", namlen, " ; Length of person label\n",
                         "XWIDE = ", xwide, " ; Matches the widest data value observed\n",
                         "; GROUPS = 0 ; Partial Credit model: in case items have different rating scales\n")
-  
+
   if (!is.null(groups)) {
     groups_string <- "ISGROUPS = *\n"
     for (i in 1:length(groups)) {
@@ -55,17 +73,17 @@ r_control_file <- function(name, data, item1, ni, id_col, ..., groups = NULL,
     }
     groups_string <- append(groups_string, "*\n")
   }
-  
+
   unique <- as.vector(as.matrix(data[1:(1+(ni-1))])) %>%
     unique() %>%
     sort()
-  
+
   codes <- c("CODES = ", ".", unique, " ; matches the data\n")
-  
+
   newsid <- toupper(id_col)
   sidlen <- (namlen - (length(demographics) + 1)) - (length(demographics))
   newsid_str <- paste0("@", newsid, " = 1E", sidlen, " ;")
-  
+
   win_demo <- c()
   if (!is.null(demographics)) {
     for (i in 1:length(demographics)) {
@@ -76,29 +94,29 @@ r_control_file <- function(name, data, item1, ni, id_col, ..., groups = NULL,
     }
     win_demo <- paste0(win_demo, collapse = "\n")
     win_demo <- paste0("\n", win_demo)
-    
+
   }
-  
+
   out_string2 <- paste0("TOTALSCORE = Yes ; Include extreme responses in reported scores\n",
                         "; Person Label variables: columns in label: columns in line\n",
                         newsid_str, win_demo,
                         "\n&END ; Item labels follow: columns in label\n")
-  
+
   item_names = c()
   for (i in 1:ni) {
     new_name <- colnames(data[i])
-    item_names <- c(item_names, paste0(new_name, " ; ", "Item ", i, " : ", i, 
+    item_names <- c(item_names, paste0(new_name, " ; ", "Item ", i, " : ", i,
                                        "-", i, "\n"))
   }
   end_names <- "END NAMES\n"
-  
+
   new <- c(out_string1, groups_string, codes, out_string2, item_names, end_names)
-  
+
   cat(new, file = paste0(name, '_cf_r.txt'), sep = "")
-  
-  write.table(data, file = paste0(name, '_cf_r.txt'), row.names = FALSE, 
+
+  write.table(data, file = paste0(name, '_cf_r.txt'), row.names = FALSE,
               col.names = FALSE,
               na = ".", sep = "", quote = FALSE, append = TRUE)
-  
+
   toc()
 }
--- R/winsteps_output.R
+++ R/winsteps_output.R
@@ -1,21 +1,37 @@
-winsteps_output <- function(ctrlfile, shortname, ..., tables = NULL, 
+#' @title winsteps_output
+#'
+#' @description Interfaces with Winsteps, creating a BATCH file to analyze
+#' a control file and export tables and files
+#'
+#' @param ctrlfile The filename of a Winsteps controlfile in the currecnt directory
+#' @param shortname Name used to save exported files
+#' @param tables A vector containing the tables to be exported from Winsteps
+#' @param files A vector containing the files to be exported from Winsteps
+#' @param dif Column names for a DIF table to be exported
+#' @param ifile Include 'ifile' for an ifile to be exported
+#' @param sfile Include 'sfile' for an sfile to be exported
+#' @return Exports select tables and files from Winsteps
+#' @export
+#' @importFrom dplyr "%>%"
+
+winsteps_output <- function(ctrlfile, shortname, ..., tables = NULL,
                             files = NULL, dif = NULL, ifile = NULL,
                             sfile = NULL) {
-  
+
   wd <- getwd()
   ctrltxt <- file.path(wd, ctrlfile)
-  
+
   if (!is.null(tables)) {
     outtables <- c()
     outtext <- c()
-    
+
     for (i in 1:length(tables)) {
-      outtext <- append(outtext, file.path(wd, paste0(shortname, "_Table_", 
+      outtext <- append(outtext, file.path(wd, paste0(shortname, "_Table_",
                                                       tables[i], ".txt")))
-      
+
       table <- tables[i]
       tableout <- list()
-      
+
       for (i in 1:(table-1)) {
         tableout <- paste0("0", tableout)
       }
@@ -23,69 +39,69 @@ winsteps_output <- function(ctrlfile, shortname, ..., tables = NULL,
       outtables <- append(outtables, templist)
     }
   }
-  
+
   if(!is.null(dif)) {
-    
+
     diftable <- "TABLES=000000000000000000000000000001"
     outdif <- c()
     difname <- c()
-    
+
     for (i in 1:length(dif)) {
-      outdif <- append(outdif, file.path(wd, paste0(shortname, "_DIF_", 
+      outdif <- append(outdif, file.path(wd, paste0(shortname, "_DIF_",
                                                     dif[i], ".txt")))
       difname <- append(difname, paste0("dif=@", dif[i]))
-      
+
     }
   }
-  
+
   if (!is.null(files)) {
     blank <- file.path(wd, "Blankoutfile.txt ")
     outfiles <- c()
-    
+
     for (i in 1:length(files)) {
-      outfiles <- append(outfiles, paste0(files[i], "=", shortname, "_", 
+      outfiles <- append(outfiles, paste0(files[i], "=", shortname, "_",
                                           files[i], ".xlsx"))
     }
     outfiles <- toString(outfiles)
   }
-  
+
   ifile_text <- ""
   sfile_text <- ""
-  
+
   if(!is.null(ifile)) {
     ifile_text <- paste0("IAFILE=", file.path(wd, paste0(ifile, ".txt")))
   }
-  
+
   if(!is.null(sfile)) {
     sfile_text <- paste0("SAFILE=", file.path(wd, paste0(sfile, ".txt")))
   }
-  
+
   first <- paste("START /w C:\\winsteps\\WINSTEPS BATCH=YES ")
-  
+
   sink(paste(shortname, "bat", sep = "."))
-  
+
   if (!is.null(tables)) {
     for (i in 1:length(outtables)) {
-      cat(paste(first, ctrltxt, outtext[i], ifile_text, sfile_text, 
+      cat(paste(first, ctrltxt, outtext[i], ifile_text, sfile_text,
                 outtables[i], sep = " "), step = "\n")
     }
   }
-  
+
   if (!is.null(files)) {
     cat(paste(first, ctrltxt, blank, outfiles), step = "\n")
   }
-  
+
   if (!is.null(dif)) {
     for (i in 1:length(dif)) {
       cat(paste(first, ctrltxt, outdif[i], difname[i], diftable), step = "\n")
     }
   }
-  
+
   cat(paste("EXIT"))
   sink()
-  
+
   shell(paste0(shortname, ".bat"))
-  
+
   unlink("Blankoutfile.txt")
   unlink(paste0(shortname, ".bat"))
 }
