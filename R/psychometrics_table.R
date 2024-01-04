0c0facbabe2ae1ed86565e6c2dbaa5130e755aab
Jeffrey <jeffreysnowden@gmail.com>
2024-01-04 10:42:19 -0600

    upading psychometrics

--- R/psychometrics_table.R
+++ R/psychometrics_table.R
@@ -42,18 +42,6 @@ psychometrics_table <- function(filename) {
     table10_data <- brio::read_lines(paste0(filename[2], "_Table_10.txt"))
     table10_data <- table10_data[!grepl("-----", table10_data)]
 
-    # table10.1_colnames <- c("Entry Number", "Total Score", "Total Count",
-    #                         "JMLE Measure", "Model S.E.", "Infit MNSQ",
-    #                         "Infit ZSTD", "Outfit MNSQ", "Outfit ZSTD",
-    #                         "PTMEASUR-AL", " Corr.", "Exp", "Exact Obs%",
-    #                         "Match Exp%", "Item")
-    #
-    # table10.1_colnames_anchor <- c("Entry Number", "Total Score", "Total Count",
-    #                                "JMLE Measure", "Model S.E.", "Infit MNSQ",
-    #                                "Infit ZSTD", "Outfit MNSQ", "Outfit ZSTD",
-    #                                "PTMEASUR-AL", " Corr.", "Exp", "Exact Obs%",
-    #                                "Match Exp%", "Displace", "Item")
-
     table10.1_end <- min(which(grepl("TABLE 10.3", table10_data) == TRUE))
 
     table10.1 <- table10_data[13:table10.1_end-4]
@@ -66,12 +54,6 @@ psychometrics_table <- function(filename) {
       select(-1, -ncol(.)) %>%
       mutate_at(c(1:3, 5:9, 11:14), as.numeric)
 
-    # if (ncol(result_df) == 15) {
-    #   colnames(result_df) <- table10.1_colnames
-    # } else if (ncol(result_df) == 16) {
-    #   colnames(result_df) <- table10.1_colnames_anchor
-    # }
-
     mean_infit <- round(mean(result_df$`X7`), 2)
     mean_outfit <- round(mean(result_df$`X9`), 2)
     #high_infit <- sum(result_df$`Infit MNSQ` > 1.3)
@@ -83,9 +65,6 @@ psychometrics_table <- function(filename) {
 
     # Extract the variable name without the .txt extension
     df_name <- sub("\\.txt$", "", tools::file_path_sans_ext(filename[2]))
-
-    # Assign the table10_data frame to a variable in the global environment
-    #assign(df_name, result_df, envir = .GlobalEnv)
 
     # Table 3 --------------------------------------------------------------
     table3_data <- brio::read_lines(paste0(filename[2], "_Table_3.txt"))
@@ -115,8 +94,6 @@ psychometrics_table <- function(filename) {
 
     psychometrics <- rbind(psychometrics, psy_line)
 
-    #assign("psy_line", psy_line, envir = .GlobalEnv)
-
     setwd(startingdir)
 
   }
@@ -128,7 +105,6 @@ psychometrics_table <- function(filename) {
 
   psychometrics <- mutate_at(psychometrics, vars(2:16), as.numeric)
 
-  #assign("psychometrics", psychometrics, envir = .GlobalEnv)
   return(psychometrics)
 
 }
