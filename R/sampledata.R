56ffa7674a04e2d0a0443cf5cb48bc00b9867bd7
Jeffrey <jeffreysnowden@gmail.com>
2024-05-24 14:17:34 -0500

    added test data

---
+++ R/sampledata.R
@@ -0,0 +1,34 @@
+#' Sample Dataset
+#'
+#' This dataset contains various Likert scale and dichotomous variables for example purposes.
+#'
+#' @name sampledata
+#' @docType data
+#' @title Sample Data
+#' @description A dataset with 18 columns containing Likert scale responses and dichotomous variables.
+#' @usage data(sampledata)
+#' @format A data frame with 100 rows and 18 variables:
+#' \describe{
+#'   \item{ID}{Unique identifier for each observation}
+#'   \item{Likert1}{Response to Likert scale question 1 (integer)}
+#'   \item{Likert2}{Response to Likert scale question 2 (integer)}
+#'   \item{Likert3}{Response to Likert scale question 3 (integer)}
+#'   \item{Likert4}{Response to Likert scale question 4 (integer)}
+#'   \item{Likert5}{Response to Likert scale question 5 (integer)}
+#'   \item{Likert6}{Response to Likert scale question 6 (integer)}
+#'   \item{Likert7}{Response to Likert scale question 7 (integer)}
+#'   \item{Likert8}{Response to Likert scale question 8 (integer)}
+#'   \item{Dicot1}{Dichotomous variable 1 (0 or 1)}
+#'   \item{Dicot2}{Dichotomous variable 2 (0 or 1)}
+#'   \item{Dicot3}{Dichotomous variable 3 (0 or 1)}
+#'   \item{Dicot4}{Dichotomous variable 4 (0 or 1)}
+#'   \item{Dicot5}{Dichotomous variable 5 (0 or 1)}
+#'   \item{Dicot6}{Dichotomous variable 6 (0 or 1)}
+#'   \item{Dicot7}{Dichotomous variable 7 (0 or 1)}
+#'   \item{Dicot8}{Dichotomous variable 8 (0 or 1)}
+#' }
+#' @source Generated for example purposes
+#' @examples
+#' data(sampledata)
+#' head(sampledata)
+"sampledata"
